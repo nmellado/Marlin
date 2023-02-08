bb6d3e8f807bf0ac125e8680d853c0dcc1e995da
Nicolas Mellado <nmellado0@gmail.com>
2023-02-08 23:46:33 +0100

    Add sensorless homing

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3152,14 +3152,16 @@
    *
    * Comment *_STALL_SENSITIVITY to disable sensorless homing for that axis.
    * @section tmc/stallguard
+   * 
+   * See https://www.youtube.com/watch?v=1U3-8aIcUm0&t=2s
    */
-  //#define SENSORLESS_HOMING // StallGuard capable drivers only
+  #define SENSORLESS_HOMING // StallGuard capable drivers only
 
   #if EITHER(SENSORLESS_HOMING, SENSORLESS_PROBING)
     // TMC2209: 0...255. TMC2130: -64...63
-    #define X_STALL_SENSITIVITY  8
+    #define X_STALL_SENSITIVITY  1
     #define X2_STALL_SENSITIVITY X_STALL_SENSITIVITY
-    #define Y_STALL_SENSITIVITY  8
+    #define Y_STALL_SENSITIVITY  1
     #define Y2_STALL_SENSITIVITY Y_STALL_SENSITIVITY
     //#define Z_STALL_SENSITIVITY  8
     //#define Z2_STALL_SENSITIVITY Z_STALL_SENSITIVITY
